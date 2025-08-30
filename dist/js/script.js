b2d293e18b451e68cbf65a250bcd0252c3a87eaa
agik20 <ardutraa40@gmail.com>
2025-08-30 15:24:01 +0700

    added transition

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,18 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("opacity-0", "translate-y-20", "-translate-x-20", "-translate-y-20");
+        entry.target.classList.add("opacity-100", "translate-y-0", "translate-x-0");
+      }
+    });
+  }, { threshold: 0.1 });
+
+  document.querySelectorAll("#portfolio .opacity-0").forEach((el) => {
+    observer.observe(el);
+  });
+});
